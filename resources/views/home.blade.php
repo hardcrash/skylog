197c592efe56c81a2808bf861c411d9ad11b634b
Curtis Klumas <curtis.klumas@gmail.com>
2025-10-16 06:32:13 -0500

    Small changes to home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,42 +5,15 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
 </head>
-<body>
 
-  # @auth
+<body>
+  <img src="../../public/images/skylog-logo.png" alt="skylog">
+  @auth
   # <p>Congrats you are logged in.</p>
   # <form action="/logout" method="POST">
   #   @csrf
   #   <button>Log out</button>
-  # </form>
 
-  # <div style="border: 3px solid black;">
-  #   <h2>Create a New Post</h2>
-  #   <form action="/create-post" method="POST">
-  #     @csrf
-  #     <input type="text" name="title" placeholder="post title">
-  #     <textarea name="body" placeholder="body content..."></textarea>
-  #     <button>Save Post</button>
-  #   </form>
-  # </div>
-
-  # <div style="border: 3px solid black;">
-  #   <h2>All Posts</h2>
-  #   @foreach($posts as $post)
-  #   <div style="background-color: gray; padding: 10px; margin: 10px;">
-  #     <h3>{{$post['title']}} by {{$post->user->name}}</h3>
-  #     {{$post['body']}}
-  #     <p><a href="/edit-post/{{$post->id}}">Edit</a></p>
-  #     <form action="/delete-post/{{$post->id}}" method="POST">
-  #       @csrf
-  #       @method('DELETE')
-  #       <button>Delete</button>
-  #     </form>
-  #   </div>
-  #   @endforeach
-  # </div>
-
-  # @else
   <div style="border: 3px solid black;">
     <h2>Register</h2>
     <form action="/register" method="POST">
@@ -51,6 +24,7 @@
       <button>Register</button>
     </form>
   </div>
+
   <div style="border: 3px solid black;">
     <h2>Login</h2>
     <form action="/login" method="POST">
@@ -60,8 +34,7 @@
       <button>Log in</button>
     </form>
   </div>
-#  @endauth
-
-
+  @endauth
 </body>
+
 </html>
